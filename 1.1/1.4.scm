5b60c1da53bda3c13a23be4d4d23f0091728c942
Gustavo <andrescabral.c@gmail.com>
2018-12-14 15:56:24 -0300

    exercise 1.4 added

---
+++ 1.1/1.4.scm
@@ -0,0 +1,9 @@
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+;This is a shorter way than writing the following procedure:
+(define (a-plus-abs-b a b)
+  ((if (> b 0) (+ a b) (- a b))))
+
+;And it becomes even shorter when the procedure has more arguments 
+;than two or the arguments' names are larger.
